8b77da9c8d17cef7439dca2d1480bb631f0f9cbc
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2025-06-25 13:27:58 -0300

    video editor

--- lib/features/editor/presentation/video_editor_screen.dart
+++ lib/features/editor/presentation/video_editor_screen.dart
@@ -1,10 +1,101 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_editor/video_editor.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoEditorScreen extends StatelessWidget {
+class VideoEditorScreen extends StatefulWidget {
   const VideoEditorScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Here is the main place ")));
+  State<VideoEditorScreen> createState() => _VideoEditorScreenState();
+}
+
+class _VideoEditorScreenState extends State<VideoEditorScreen> {
+  final ImagePicker _picker = ImagePicker();
+  VideoEditorController? _videoEditorController;
+  VideoPlayerController? _videoPlayerController;
+  bool canShowEditor = false;
+  List<String> trimmedVideos = [];
+  bool isSeeking = false;
+
+  Future<void> _pickVideo() async {
+    final XFile? file = await _picker.pickVideo(source: ImageSource.gallery);
+    if (file != null) {
+      _videoEditorController = VideoEditorController.file(
+        File(file.path),
+        minDuration: const Duration(seconds: 1),
+        maxDuration: const Duration(seconds: 53),
+      );
+      _videoPlayerController = VideoPlayerController.file(File(file.path))
+        ..setLooping(true);
+
+      try {
+        await Future.wait([
+          _videoEditorController!.initialize(),
+          _videoPlayerController!.initialize(),
+        ]);
+        _videoPlayerController!.addListener(() {
+          if (_videoPlayerController!.value.position >=
+              _videoEditorController!.endTrim) {
+            _videoPlayerController!.pause();
+          }
+        });
+        setState(() {
+          canShowEditor = true;
+        });
+      } catch (e) {
+        print("errror: $e");
+      }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Video Editor',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.black,
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "Select a video to start editing",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 25),
+                child: ElevatedButton(
+                  onPressed: _pickVideo,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 15,
+                    ),
+                  ),
+                  child: Text(
+                    "Import Video",
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:capcut/features/editor/presentation/video_editor_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,12 +8,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Capcut Clone'),
+        title: const Text(
+          'Capcut Clone',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
       body: Center(
-        child: ElevatedButton(onPressed: () {}, child: Text("New Project")),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 15,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => VideoEditorScreen()),
+                );
+              },
+              child: Text("New Project"),
+            ),
+            SizedBox(height: 30),
+            Text(
+              "Your Projects",
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+          ],
+        ),
       ),
     );
   }
